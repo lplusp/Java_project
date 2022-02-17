65b4033f41a31ccd4e78bf19a6740aa983e5b8d3
Валерия Прозорова <leraprozorova1911@gmail.com>
2022-02-17 23:45:47 +0300

    Lesson5

---
+++ src/main/java/Class2.java
@@ -0,0 +1,8 @@
+public class Class2 {
+    public String constructString() {
+        Integer i = 123456;
+        System.out.println("---" + i + "---");
+        return String.valueOf(i);
+    }
+}
+
